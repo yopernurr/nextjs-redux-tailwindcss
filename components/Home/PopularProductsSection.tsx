c611764fb475d848337a35770f63c9a880347e08
yogs <yopernurr@gmail.com>
2020-10-15 15:40:49 +0700

    Loading popular product

--- components/Home/PopularProductsSection.tsx
+++ components/Home/PopularProductsSection.tsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import Skeleton from 'react-loading-skeleton';
 
 const PopularProductsSection = () => {
+
+  const [loading, setloading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setloading(false)
+    }, 2000);
+  }, []);
+
   const products = [
     {
       image: 'https://via.placeholder.com/300x300.png',
@@ -29,13 +39,22 @@ const PopularProductsSection = () => {
       <span className="w-full font-medium">POPULAR PRODUCTS</span>
       <div className="w-full flex mt-0 flex-wrap">
         {
-          products.map(product => (
-            <div key={product.name} className="w-1/2 flex flex-wrap p-2 mt-4 hover:shadow-lg">
-              <img className="w-full" src={product.image} alt=""/>
-              <div className="w-full mt-2 font-medium text-sm">{product.price}</div>
-              <div className="w-full text-sm">{product.name}</div>
-            </div>
-          ))
+          !loading
+          ?
+            products.map(product => (
+              <div key={product.name} className="w-1/2 flex flex-wrap p-2 mt-4 hover:shadow-lg">
+                <img className="w-full" src={product.image} alt=""/>
+                <div className="w-full mt-2 font-medium text-sm">{product.price}</div>
+                <div className="w-full text-sm">{product.name}</div>
+              </div>
+            ))
+          :
+            [1,2,3,4].map(v => (
+              <div key={v} className="w-1/2 flex flex-wrap p-2 mt-4">
+                <Skeleton width={320} height={320} className="text-center"/>
+                <Skeleton width={100} height={10} />
+              </div>
+            ))
         }
       </div>
     </div>
